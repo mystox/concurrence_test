5db006dc1d6ac5c993889cebdf61bfb847e9c1bf
mystox <mystox@163.com>
2018-03-06 16:11:25 +0800

    This is a concurrence test demo by mystox. You can get some code technology in this projects.

--- src/main/java/tech/mystox/kafka/KafkaTestApplication.java
+++ src/main/java/tech/mystox/kafka/KafkaTestApplication.java
@@ -12,7 +12,6 @@ import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfigurat
 
 public class KafkaTestApplication
 {
-
 	public static void main(String[] args) {
 		SpringApplication.run(KafkaTestApplication.class, args);
 	}
