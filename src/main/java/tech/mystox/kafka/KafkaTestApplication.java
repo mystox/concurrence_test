94d0bfdfd20dc1ebadbdf1d21d62a54748cfdcf4
mystox <mystox@163.com>
2018-03-28 17:40:02 +0800

    一些自定义配置文件相关的测试

--- src/main/java/tech/mystox/kafka/KafkaTestApplication.java
+++ src/main/java/tech/mystox/kafka/KafkaTestApplication.java
@@ -1,14 +1,9 @@
 package tech.mystox.kafka;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
 
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class,MongoAutoConfiguration.class,MongoDataAutoConfiguration.class})
+//@SpringBootApplication(exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class,MongoAutoConfiguration.class,MongoDataAutoConfiguration.class})
 
 public class KafkaTestApplication
 {
